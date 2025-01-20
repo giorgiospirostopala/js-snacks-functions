33e87306c0475c4e3f3b21ad5e435170f7acb1ff
giorgiospirostopala <gio.topala@gmail.com>
2025-01-20 17:42:28 +0100

    add: 05-done (con arrow function)

--- snack-funzioni/05_bonus/script.js
+++ snack-funzioni/05_bonus/script.js
@@ -5,14 +5,29 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const names = 'Mario';
 
 // Dichiara la funzione qui.
+const tiSaluto = (nome) => {
 
+    const oraCorrente = new Date().getHours();
+    
+    //dichiaro la variabile "saluto" che costituirà la somma di ${name} + la parolina richiesta in base all'ora
+    let saluto;
+
+    if (oraCorrente >= 0 && oraCorrente < 13) {
+        saluto = "Buongiorno " + nome;
+    } else if (oraCorrente >= 13 && oraCorrente < 17) {
+        saluto = "Buon pomeriggio " + nome;
+    } else {
+        saluto = "Buonasera " + nome;
+    }
+
+    return saluto;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let restituisciSaluto = tiSaluto(names);
+console.log(restituisciSaluto);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
