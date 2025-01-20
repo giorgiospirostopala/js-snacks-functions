febb8c6aaaf44ca045de4d1ad350adbfc390d3f3
giorgiospirostopala <gio.topala@gmail.com>
2025-01-20 17:04:09 +0100

    04-done

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -3,11 +3,32 @@
 const word = 'javascript';
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui. Deve:
+//  - accettare una stringa
+//  - riconoscere e contare quante vocali contiene
+//  - restituire il numero di vocali contate
+
+function trovaNumeroVocali(stringa){
+    // definisco l'array con le vocali
+    const vocali = ["a", "e", "i", "o", "u"];
+    // definisco la variabile che indicherà il numero di vocali contate, a partire da zero
+    let vocaliContate = 0;
+
+    // uso un ciclo for() per controllare ogni lettera della stringa
+    for (let i = 0; i < stringa.length; i++){
+        // uso if per verificare se la lettera in questione è una vocale; se si la conteggio
+        if (vocali.includes(stringa[i])){
+            vocaliContate++;
+        }
+    }
+
+    return vocaliContate;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let esitoFunzione = trovaNumeroVocali(word);
+console.log(`Le vocali contenute in ${word} sono ${esitoFunzione}`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
