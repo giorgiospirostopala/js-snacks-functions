df4acccd9821f576c15730ac926fe13f0e88c625
giorgiospirostopala <gio.topala@gmail.com>
2025-01-20 16:29:36 +0100

    add: 02-done

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function trovaIniziali(nomi){
+
+    const iniziali = [];
+    for (let i = 0; i < nomi.length; i++) {
+        iniziali.push(nomi[i][0]);
+    }
+    
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const iniziali = trovaIniziali(names);
+console.log(iniziali);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
