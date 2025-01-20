2320d50cf40475af1a03d8608b43237f6329c082
giorgiospirostopala <gio.topala@gmail.com>
2025-01-20 16:47:20 +0100

    add: 03-done

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -1,13 +1,26 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const letteraDaCercare = "A";
 
 // Dichiara la funzione qui.
+function trovaParoleConIniziale(nomi, iniziale){
+
+    let paroleTrovate = [];
+    for (let i = 0; i < nomi.length; i++){
+        if (nomi[i][0] === iniziale){
+            paroleTrovate.push(nomi[i]);
+        }
+    }
+
+    return paroleTrovate;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let esito = trovaParoleConIniziale(names, letteraDaCercare);
+console.log(esito);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
